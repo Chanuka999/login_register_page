9fa01ed01d66f543f2763e58c1f8662873a7741c
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-26 00:51:34 +0530

    feat: add main application structure with routing for login and signup

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import Signup from "./Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
