5df324500aa2872a0d4e9b8f2b22f5ed1a4aa861
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-26 13:14:06 +0530

    private route method created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,33 @@ import Signup from "./Signup";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/register" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route
+            path="/register"
+            element={
+              <PrivateRoute>
+                <Signup />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
